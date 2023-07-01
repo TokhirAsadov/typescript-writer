41ef53c6cfdf2714004162f23813a1a286696e99
Bismillah <guvalakat1603@gmail.con>
2023-07-01 20:28:50 +0500

    api dashboard page

---
+++ src/pages/api/api-key/create.ts
@@ -0,0 +1,14 @@
+import {NextApiRequest, NextApiResponse} from "next";
+
+const handler = async (
+    req: NextApiRequest,
+    res: NextApiResponse
+) => {
+    try {
+
+    }catch (e) {
+
+    }
+}
+
+export default handler;
